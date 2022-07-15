41fb152e101ea2ae269954c09cfd1845879c3449
zsoon96 <pres0616@gmail.com>
2022-07-16 00:43:16 +0900

    View 생성

--- src/main/java/hello/springstart/SpringStartApplication.java
+++ src/main/java/hello/springstart/SpringStartApplication.java
@@ -3,6 +3,7 @@ package hello.springstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// springboot는 내장형 톰캣(WAS)을 사용하기 때문에 별도의 톰캣 설치나 버전 관리를 해주지 않아도 되서 편리함
 @SpringBootApplication
 public class SpringStartApplication {
 
---
+++ src/main/java/hello/springstart/controller/HelloController.java
@@ -0,0 +1,18 @@
+package hello.springstart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// 웹어플리케이션에서 가장 첫번째의 진입점
+@Controller
+public class HelloController {
+
+    // 실행 시 가장 먼저
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "spring!!");
+        // view resolver가 해당 이름으로 된 html 파일을 찾아서 값 전달 ( == hello.html )
+        return "hello";
+    }
+}
