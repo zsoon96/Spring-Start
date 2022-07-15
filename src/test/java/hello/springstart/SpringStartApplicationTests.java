18603283e0c25a679c07d7582be9882ad67e5951
zsoon96 <pres0616@gmail.com>
2022-07-15 23:25:07 +0900

    프로젝트 생성

---
+++ src/test/java/hello/springstart/SpringStartApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
