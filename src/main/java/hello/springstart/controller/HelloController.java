127867c5a13a81d200e6fe7dd60f900d15b1911c
zsoon96 <pres0616@gmail.com>
2022-07-16 01:33:14 +0900

    devtools 설정 추가

--- src/main/java/hello/springstart/controller/HelloController.java
+++ src/main/java/hello/springstart/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
     // 실행 시 가장 먼저
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "spring!!");
+        model.addAttribute("data", "soon!!");
         // view resolver가 해당 이름으로 된 html 파일을 찾아서 값 전달 ( == hello.html )
         return "hello";
     }
