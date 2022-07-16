32ce1b82933a4b8444a6aead1441926da7fdaa54
zsoon96 <pres0616@gmail.com>
2022-07-16 14:00:22 +0900

    MVC와 템플릿 엔진

--- src/main/java/hello/springstart/controller/HelloController.java
+++ src/main/java/hello/springstart/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.springstart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // 웹어플리케이션에서 가장 첫번째의 진입점
 @Controller
@@ -14,5 +15,11 @@ public class HelloController {
         model.addAttribute("data", "soon!!");
         // view resolver가 해당 이름으로 된 html 파일을 찾아서 값 전달 ( == index.html )
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-templates";
     }
 }
